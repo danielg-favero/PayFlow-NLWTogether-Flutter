e2869f4cd113dca336724331abacd536b50cad83
danielg-favero <danielfavero17@gmail.com>
2022-03-17 15:06:18 -0300

    feat: adicionado exemplo de contador com flutter

---
+++ README.md
@@ -0,0 +1,5 @@
+# payflow_nlw_together_flutter
+
+## Projeto Payflow
+
+Projeto desenvolvido na NLW Together da Rocketseat relacionado a Flutter.
---
+++ android/app/src/main/kotlin/com/example/payflow_nlw_together_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.payflow_nlw_together_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
